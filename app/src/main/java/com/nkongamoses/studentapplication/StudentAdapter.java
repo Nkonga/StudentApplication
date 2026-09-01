4a48513b47a700f496a7c1c6abe848d7024fb3c7
Moses Nkonga <nkongamoses@gmail.com>
2026-09-01 08:31:05 +0200

    Add student RecyclerView adapter

---
+++ app/src/main/java/com/nkongamoses/studentapplication/StudentAdapter.java
@@ -0,0 +1,67 @@
+package com.nkongamoses.studentapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentViewHolder> {
+
+    private List<Student> studentList;
+
+    public StudentAdapter(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    @NonNull
+    @Override
+    public StudentViewHolder onCreateViewHolder(
+            @NonNull ViewGroup parent,
+            int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_student, parent, false);
+
+        return new StudentViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(
+            @NonNull StudentViewHolder holder,
+            int position) {
+
+        Student student = studentList.get(position);
+
+        holder.tvStudentName.setText(student.getFullName());
+        holder.tvStudentId.setText("Student ID: " + student.getStudentId());
+        holder.tvStudentGrade.setText("Grade: " + student.getGrade());
+        holder.tvStudentGender.setText("Gender: " + student.getGender());
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentList.size();
+    }
+
+    public static class StudentViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvStudentName;
+        TextView tvStudentId;
+        TextView tvStudentGrade;
+        TextView tvStudentGender;
+
+        public StudentViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvStudentName = itemView.findViewById(R.id.tvStudentName);
+            tvStudentId = itemView.findViewById(R.id.tvStudentId);
+            tvStudentGrade = itemView.findViewById(R.id.tvStudentGrade);
+            tvStudentGender = itemView.findViewById(R.id.tvStudentGender);
+        }
+    }
+}
